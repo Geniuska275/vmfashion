ba0338df423feb3ac49ce689a1216c742acca87e
Geniuska275 <aigbojie2020@gmail.com>
2025-06-04 22:46:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import viteLogo from './website.avif'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+        
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+      
+       
+    </>
+  )
+}
+
+export default App
