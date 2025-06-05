b511433c3ad462366f0b57c7817f6f8c7c88f13c
Geniuska275 <aigbojie2020@gmail.com>
2025-06-05 05:33:11 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,18 @@ function App() {
     
         
           <img src={viteLogo} className="logo" alt="Vite logo" />
+          <h1 style={{
+            fontSize:"30px",
+            fontFamily:"Poppins"
+          }}>we are building a fantastic website,stay tuned.</h1>
+          <h2
+          style={{
+            fontSize:"25px",
+            fontFamily:"Poppins"
+          }}
+          >This website is under construction,and willl be up in few days</h2>
+
+
       
        
     </>
