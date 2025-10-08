00f9a40da07af004653374508481b06a362f6234
Geniuska275 <aigbojie2020@gmail.com>
2025-10-08 05:53:05 +0100

    maintenance

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,20 +5,20 @@ import ProductListing from '../components/ProductListing'
 function Homepage() {
   return (
     <div>
-        
-              {/* Breadcrumb */}
-              <div className="container mx-auto px-4 py-4">
+        <h1>This website is undergoing maintenance...</h1>
+            
+              {/* <div className="container mx-auto px-4 py-4">
                <div className="flex items-center text-sm">
                   <a href="/" className="text-gray-600">
                     VM Fashion  </a>           <span className="mx-2">→</span>
                    <span className="font-medium">Women Ethnic Dresses</span>
                  </div>
                </div>
-               {/* Category Circles */}     
+              
                 <CategoryCircles />
         
-               {/* <ProductGrid/> */}
-              <ProductListing/>
+               
+              <ProductListing/> */}
     </div>
   )
 }
