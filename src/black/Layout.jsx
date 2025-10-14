d03386bbff9d4e1d390422071c06431928b57aa4
Geniuska275 <aigbojie2020@gmail.com>
2025-10-14 12:25:45 +0100

    updated

--- src/black/Layout.jsx
+++ src/black/Layout.jsx
@@ -12,10 +12,8 @@ function Layout({ children }) {
   }, [])
 
   return (
-    <div className="min-h-screen bg-white text-amber-300">
-      <h1 className="text-black font-bold text-3xl text-center">This website is undergoing maintenance... </h1>
-      <p className="text-black font-bold  text-center">Kindly contact the developer for more info.</p>
-      {/* <div className="container mx-auto px-4 py-8">
+    <div className="min-h-screen bg-black text-amber-300">
+      <div className="container mx-auto px-4 py-8">
         <header className="flex flex-col md:flex-row items-center justify-between mb-12">
           <Logo />
         </header>
@@ -25,7 +23,7 @@ function Layout({ children }) {
           <main className="w-full md:w-3/4">{children}</main>
         </div>
         <Footer className="mt-12" />
-      </div> */}
+      </div>
     </div>
   )
 }
